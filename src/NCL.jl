c7880d0e97392c9deacb3e3828c976a10d78eeda
Alberto De Marchi <aldmarchi@gmail.com>
2024-07-09 09:03:56 +0200

    add list of available solvers

--- src/NCL.jl
+++ src/NCL.jl
@@ -1,5 +1,3 @@
-# TODO: make NLPModelsKnitro optional
-
 module NCL
 
 using LinearAlgebra
@@ -9,7 +7,29 @@ using NLPModels
 using SolverCore
 using KNITRO
 using NLPModelsIpopt
-using NLPModelsKnitro
+
+global available_solvers = [:ipopt]
+
+if KNITRO.has_knitro()
+    using NLPModelsKnitro
+    push!(available_solvers, :knitro)
+end
+
+
+"""
+    _check_available_solver(solver::Symbol)
+
+Return an error if `solver` is not in `NCL.available_solvers`
+"""
+function _check_available_solver(solver::Symbol)
+    if !(solver in available_solvers)
+        s = "`solver` must be one of these: "
+        for x in available_solvers
+            s *= "`$x`, "
+        end
+        error(s[1:(end-2)])
+    end
+end
 
 include("NCLModel.jl")
 include("NCLSolve.jl")
